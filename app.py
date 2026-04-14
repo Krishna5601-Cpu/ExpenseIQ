906e1b4c483c128ce8fd0fda6d107881dad5ce9e
Krishna <krishnachauhan2724@gmail.com>
2026-04-14 22:16:25 +0530

    feat: added basic funtionality - CRUD operations, basic frontend, connecting everything and base structure

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template, redirect
 from config import Config
+from services import add_expense, get_all_expenses, delete_expense
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -7,7 +10,33 @@ app.config.from_object(Config)
 
 @app.route("/")
 def home():
-    return "💵🧠 ExpenseIQ is running"
+    expenses = get_all_expenses()
+    return render_template("index.html", expenses=expenses)
+
+
+@app.route("/add-expense", methods=["POST"])
+def create_expense():
+    date = request.form.get("date")
+
+    if not date:
+        date = datetime.now().strftime("%Y-%m-%d")
+
+    data = {
+        "amount": int(request.form.get("amount")),
+        "category": request.form.get("category"),
+        "description": request.form.get("description"),
+        "date": date,
+    }
+
+    add_expense(data)
+
+    return redirect("/")
+
+
+@app.route("/delete/<expense_id>")
+def delete(expense_id):
+    delete_expense(expense_id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
---
+++ services.py
@@ -0,0 +1,47 @@
+import json
+import os
+import uuid
+
+FILE_PATH = "expenses.json"
+
+
+def load_expenses():
+    if not os.path.exists(FILE_PATH):
+        return []
+
+    with open(FILE_PATH, "r") as f:
+        return json.load(f)
+
+
+def save_expenses(expenses):
+    with open(FILE_PATH, "w") as f:
+        json.dump(expenses, f, indent=4)
+
+
+def add_expense(data):
+    expenses = load_expenses()
+
+    expense = {
+        "id": str(uuid.uuid4()),
+        "amount": data.get("amount"),
+        "category": data.get("category"),
+        "description": data.get("description"),
+        "date": data.get("date"),
+    }
+
+    expenses.append(expense)
+    save_expenses(expenses)
+
+    return expense
+
+
+def delete_expense(expense_id):
+    expenses = load_expenses()
+
+    updated_expenses = [expense for expense in expenses if expense["id"] != expense_id]
+
+    save_expenses(updated_expenses)
+
+
+def get_all_expenses():
+    return load_expenses()
